59851384f9b5cb2cb749fa15396b2c71d7e2b13f
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-06 09:55:02 +0100

    Added script for linting

---
+++ scripts/lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$(git rev-parse --show-toplevel)" || (echo "Couldn't find project folder. Please check your working dir."; exit 1)
+
+flake8 ./ensysmod
+flake8 ./tests
+
+echo "Flake8 complete. Check for code smells above."
