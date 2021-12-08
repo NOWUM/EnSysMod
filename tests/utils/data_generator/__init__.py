0073264c3a4c774b0bfc897ad6778c2ad9eb578a
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-08 22:29:30 +0100

    Fixed import issue in __init__.py

--- tests/utils/data_generator/__init__.py
+++ tests/utils/data_generator/__init__.py
@@ -1,7 +1,7 @@
 from .datasets import random_existing_dataset, random_dataset_create, fixed_existing_dataset
 from .energy_commodities import random_existing_energy_commodity, fixed_existing_energy_commodity, \
     random_energy_commodity_create
-from .energy_conversions import random_existing_energy_commodity, fixed_existing_energy_commodity, \
+from .energy_conversions import random_existing_energy_conversion, fixed_existing_energy_conversion, \
     random_energy_conversion_create
 from .energy_sinks import random_existing_energy_sink, fixed_existing_energy_sink, random_energy_sink_create
 from .energy_sources import random_existing_energy_source, fixed_existing_energy_source, random_energy_source_create
