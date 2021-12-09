a22ed196698cfcf3a7bf2be7a46c96e73573acc4
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-09 13:20:24 +0100

    Renamed region.json to regions.json

--- ensysmod/core/file_upload.py
+++ ensysmod/core/file_upload.py
@@ -15,7 +15,7 @@ def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Sessi
 
     The zip archive must contain the following files:
     - commodities.json representing a List[CommodityCreate]
-    - region.json representing a List[RegionCreate]
+    - regions.json representing a List[RegionCreate]
 
     The zip archive can contain the following folders:
     - conversions
@@ -29,21 +29,32 @@ def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Sessi
     :param db: Database session
     """
 
+    if "commodities.json" not in zip_archive.namelist():
+        raise ValueError("Zip archive must contain commodities.json")
+
+    if "regions.json" not in zip_archive.namelist():
+        raise ValueError("Zip archive must contain regions.json")
+
+    # process region.json
+    region_file = zip_archive.open("regions.json")
+    process_regions_file(region_file, dataset_id, db)
+
     # print all files in the zip archive
     for file in zip_archive.namelist():
         print(file)
 
 
-def process_region_file(region_file: TemporaryFile, dataset_id: int, db: Session):
+def process_regions_file(regions_file: TemporaryFile, dataset_id: int, db: Session):
     """
     Processes a region file and adds the regions to the dataset in database.
 
-    :param region_file: File to process
+    :param regions_file: File to process
     :param dataset_id: ID of the dataset to add the regions to
     :param db: Database session
     """
     # map region_file.json to List[RegionCreate]
-    regions: List[RegionCreate] = json.load(region_file)
+    regions: List[RegionCreate] = json.load(regions_file)
+    print(regions)
     for region in regions:
         existing_region = crud.region.get_by_dataset_and_name(db, dataset_id=dataset_id, name=region.name)
         if existing_region is not None:
