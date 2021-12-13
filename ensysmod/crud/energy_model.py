d1aa997086bb54f73e199545a0c1d16ea771a74e
Melanie Ecker <melanie.ecker@alumni.fh-aachen.de>
2021-12-13 12:10:05 +0100

    Updated Crud for energy_model

--- ensysmod/crud/energy_model.py
+++ ensysmod/crud/energy_model.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy.orm import Session
 
@@ -13,8 +13,17 @@ class CRUDEnergyModel(CRUDBase[EnergyModel, EnergyModelCreate, EnergyModelUpdate
     CRUD operations for EnergyModel
     """
 
-    def get_by_name(self, db: Session, *, name: str) -> Optional[EnergyModel]:
-        return db.query(EnergyModel).filter(EnergyModel.name == name).first()
+    def get_multi_by_dataset(
+            self, db: Session, *, skip: int = 0, limit: int = 100, dataset_id: int
+    ) -> List[EnergyModel]:
+        return db.query(EnergyModel) \
+            .filter(EnergyModel.ref_dataset == dataset_id) \
+            .offset(skip).limit(limit).all()
+
+    def get_by_dataset_and_name(self, db: Session, *, dataset_id: int, name: str) -> Optional[EnergyModel]:
+        return db.query(EnergyModel) \
+            .filter(EnergyModel.name == name and EnergyModel.ref_dataset == dataset_id) \
+            .first()
 
 
 energy_model = CRUDEnergyModel(EnergyModel)
