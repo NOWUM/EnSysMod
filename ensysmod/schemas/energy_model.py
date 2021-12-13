68a232aa8bb3cfaaaf5021190f53226fd9c05c5b
Melanie Ecker <melanie.ecker@alumni.fh-aachen.de>
2021-12-13 11:48:39 +0100

    Added schema for energy_model

---
+++ ensysmod/schemas/energy_model.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ensysmod.schemas import Dataset
+
+
+class EnergyModelBase(BaseModel):
+    """
+    Shared properties for an energy model. Used as a base class for all schemas.
+    """
+    name: str
+    yearly_co2_limit: int
+    description: Optional[str] = None
+
+
+class EnergyModelCreate(EnergyModelBase):
+    """
+    Properties to receive via API on creation of an energy model.
+    """
+    ref_dataset: int
+
+
+class EnergyModelUpdate(EnergyModelBase):
+    """
+    Properties to receive via API on update of an energy model.
+    """
+    name: Optional[str] = None
+    yearly_co2_limit: Optional[int] = None
+
+
+class EnergyModel(EnergyModelBase):
+    """
+    Properties to return via API for an energy model.
+    """
+    id: int
+    dataset: Dataset
+
+    class Config:
+        orm_mode = True
