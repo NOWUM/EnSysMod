d46e202a50f231a4ccb4a703eaca163a89473c82
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-15 23:38:36 +0100

    Added proof of concept code

--- ensysmod/core/file_upload.py
+++ ensysmod/core/file_upload.py
@@ -6,7 +6,8 @@ from zipfile import ZipFile
 from sqlalchemy.orm import Session
 
 from ensysmod import crud
-from ensysmod.schemas import RegionCreate, RegionUpdate, EnergyCommodityCreate, EnergyCommodityUpdate
+from ensysmod.schemas import RegionCreate, RegionUpdate, EnergyCommodityCreate, EnergyCommodityUpdate, \
+    EnergyConversionCreate, EnergyConversionUpdate
 
 
 def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Session):
@@ -43,9 +44,14 @@ def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Sessi
     commodity_file = zip_archive.open("commodities.json")
     process_commodities_file(commodity_file, dataset_id, db)
 
+    # process conversions
+    for conversion_folder in retrieve_subfolders(zip_archive, "conversions/"):
+        process_single_conversion(zip_archive, conversion_folder, dataset_id, db)
+
     # print all files in the zip archive
     for file in zip_archive.namelist():
-        print(file)
+        # print(file)
+        pass
 
 
 def process_regions_file(regions_file: TemporaryFile, dataset_id: int, db: Session):
@@ -105,3 +111,52 @@ def process_commodities_file(commodities_file: TemporaryFile, dataset_id: int, d
             print(f"Commodity {commodity.name} already exists in database. Updating...")
             update = EnergyCommodityUpdate(**commodity.dict())
             crud.energy_commodity.update(db, obj_in=update, db_obj=existing_commodity)
+
+
+def retrieve_subfolders(zip_archive: ZipFile, folder_name: str) -> List[str]:
+    """
+    Checks if the folder_name exits inside the zip_archive and converts every sub folder
+    """
+    print(f"Processing folder {folder_name}")
+    if folder_name not in zip_archive.namelist():
+        # raise ValueError(f"Folder {folder_name}
+        print(f"Folder {folder_name} doesn't exists in zip archive. Skipping...")
+        return
+
+    for sub_folder in zip_archive.open(folder_name):
+        print(f"Processing {sub_folder}")
+        yield sub_folder
+
+
+def process_single_conversion(zip_archive: ZipFile, folder_name: str, dataset_id: int, db: Session):
+    """
+    TODO
+
+    :param zip_archive: Zip archive to process
+    :param folder_name: Name of the folder to process
+    :param dataset_id: ID of the dataset to add the conversions to
+    :param db: Database session
+    """
+
+    def map_conversion(json_dict: Dict) -> EnergyConversionCreate:
+        """
+        Maps a json dict to a EnergyCommodityCreate object.
+        """
+        json_dict["ref_dataset"] = dataset_id
+        return EnergyConversionCreate.parse_obj(json_dict)
+
+    conversion_file = zip_archive.open(folder_name + "/conversion.json")
+    if conversion_file is None:
+        raise ValueError(f"Conversion file not found in {folder_name}.")
+
+    conversion = json.load(conversion_file, object_hook=lambda d: map_conversion(d))
+    existing_conversion = crud.energy_conversion.get_by_dataset_and_name(db, dataset_id=dataset_id,
+                                                                         name=conversion.name)
+    if existing_conversion is None:
+        print(f"Commodity {conversion.name} doesn't exists in dataset {dataset_id}. Creating...")
+        existing_conversion = crud.energy_conversion.create(db, obj_in=conversion)
+    else:
+        print(f"Commodity {conversion.name} already exists in database. Updating...")
+        update = EnergyConversionUpdate(**conversion.dict())
+        existing_conversion = crud.energy_conversion.update(db, obj_in=update, db_obj=existing_conversion)
+
