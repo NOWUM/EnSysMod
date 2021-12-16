6c1cab62715199aedca9c62b7f8f332fc37818fc
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-16 02:42:07 +0100

    Added file upload for component json files Refactored file upload for regions.json and commodities.json (Used advantage of new crud abstraction layer)

--- ensysmod/core/file_upload.py
+++ ensysmod/core/file_upload.py
@@ -1,13 +1,42 @@
 import json
 from tempfile import TemporaryFile
-from typing import List, Dict
+from typing import List, Dict, Any, Type
 from zipfile import ZipFile
 
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import crud
 
-from ensysmod import crud
-from ensysmod.schemas import RegionCreate, RegionUpdate, EnergyCommodityCreate, EnergyCommodityUpdate, \
-    EnergyConversionCreate, EnergyConversionUpdate
+from ensysmod import crud, schemas
+from ensysmod.crud.base_depends_component import CRUDBaseDependsComponent
+from ensysmod.crud.base_depends_dataset import CRUDBaseDependsDataset
+
+
+def create_or_update(crud_repo: CRUDBaseDependsDataset, db: Session, request: Any):
+    """
+    This function creates or updates an object inside the given crud repository.
+
+    :param crud_repo: The crud repository.
+    :param db: The database session.
+    :param request: The request.
+    :return: The created or updated object.
+    """
+    existing_object = crud_repo.get_by_dataset_and_name(db, dataset_id=request.ref_dataset, name=request.name)
+    if existing_object is None:
+        print(f"{request.name} doesn't exists in dataset {request.ref_dataset}. Creating...")
+        return crud_repo.create(db, obj_in=request)
+    else:
+        print(f"{request.name} already exists in database. Updating...")
+        return crud_repo.update(db, obj_in=request, db_obj=existing_object)
+
+
+def map_with_dataset_id(create_model: Type[BaseModel], json_dict: Dict, dataset_id: int):
+    """
+    Maps a json dict to a dict with the ref_dataset key set to the given dataset_id.
+    """
+    json_dict["ref_dataset"] = dataset_id
+    print(json_dict)
+    return create_model.parse_obj(json_dict)
 
 
 def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Session):
@@ -36,127 +65,66 @@ def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Sessi
     if "regions.json" not in zip_archive.namelist():
         raise ValueError("Zip archive must contain regions.json")
 
-    # process region.json
-    region_file = zip_archive.open("regions.json")
-    process_regions_file(region_file, dataset_id, db)
-
-    # process commodities.json
-    commodity_file = zip_archive.open("commodities.json")
-    process_commodities_file(commodity_file, dataset_id, db)
+    # process region.json and commodities.json
+    process_list_file(zip_archive.open("regions.json"), db, dataset_id, crud.region, schemas.RegionCreate)
+    process_list_file(zip_archive.open("commodities.json"), db, dataset_id, crud.energy_commodity,
+                      schemas.EnergyCommodityCreate)
 
     # process conversions
-    for conversion_folder in retrieve_subfolders(zip_archive, "conversions/"):
-        process_single_conversion(zip_archive, conversion_folder, dataset_id, db)
+    process_components_folder(zip_archive, folder_name="conversions/", component_file_name="conversion.json",
+                              dataset_id=dataset_id, db=db, crud_repo=crud.energy_conversion,
+                              create_model=schemas.EnergyConversionCreate)
+    process_components_folder(zip_archive, folder_name="sinks/", component_file_name="sink.json",
+                              dataset_id=dataset_id, db=db, crud_repo=crud.energy_sink,
+                              create_model=schemas.EnergySinkCreate)
+    process_components_folder(zip_archive, folder_name="sources/", component_file_name="source.json",
+                              dataset_id=dataset_id, db=db, crud_repo=crud.energy_source,
+                              create_model=schemas.EnergySourceCreate)
+    process_components_folder(zip_archive, folder_name="storages/", component_file_name="storage.json",
+                              dataset_id=dataset_id, db=db, crud_repo=crud.energy_storage,
+                              create_model=schemas.EnergyStorageCreate)
+    process_components_folder(zip_archive, folder_name="transmissions/", component_file_name="transmission.json",
+                              dataset_id=dataset_id, db=db, crud_repo=crud.energy_transmission,
+                              create_model=schemas.EnergyTransmissionCreate)
 
-    # print all files in the zip archive
-    for file in zip_archive.namelist():
-        # print(file)
-        pass
 
-
-def process_regions_file(regions_file: TemporaryFile, dataset_id: int, db: Session):
+def process_list_file(file: TemporaryFile, db: Session, dataset_id: int, crud_repo: CRUDBaseDependsComponent,
+                      create_model: Type[BaseModel]):
     """
-    Processes a region file and adds the regions to the dataset in database.
+    Processes a file containing a list of objects.
+    Each object gets a ref_dataset key set to the given dataset_id and gets created or updated in the database.
 
-    :param regions_file: File to process
-    :param dataset_id: ID of the dataset to add the regions to
+    :param file: File to process
     :param db: Database session
+    :param dataset_id: ID of the dataset to add the components to
+    :param crud_repo: CRUD repository to use
+    :param create_model: Create model to use
     """
-
-    def map_region(json_dict: Dict) -> RegionCreate:
-        """
-        Maps a json dict to a RegionCreate object.
-        """
-        json_dict["ref_dataset"] = dataset_id
-        return RegionCreate.parse_obj(json_dict)
-
-    regions: List[RegionCreate] = json.load(regions_file,
-                                            object_hook=lambda d: map_region(d))
-    for region in regions:
-        existing_region = crud.region.get_by_dataset_and_name(db, dataset_id=dataset_id, name=region.name)
-        if existing_region is None:
-            print(f"Region {region.name} doesn't exists in dataset {dataset_id}. Creating...")
-            crud.region.create(db, obj_in=region)
-        else:
-            print(f"Region {region.name} already exists in database. Updating...")
-            update = RegionUpdate(**region.dict())
-            crud.region.update(db, obj_in=update, db_obj=existing_region)
+    dicts: List[dict] = json.load(file)
+    for single_dict in dicts:
+        create_or_update(crud_repo, db, map_with_dataset_id(create_model, single_dict, dataset_id))
 
 
-def process_commodities_file(commodities_file: TemporaryFile, dataset_id: int, db: Session):
+def process_components_folder(zip_archive: ZipFile,
+                              folder_name: str, component_file_name: str,
+                              dataset_id: int,
+                              db: Session,
+                              crud_repo: CRUDBaseDependsComponent, create_model: Type[BaseModel]):
     """
-    Processes a commodities file and adds the commodities to the dataset in database.
-
-    :param commodities_file: File to process
-    :param dataset_id: ID of the dataset to add the regions to
-    :param db: Database session
+    Processes a folder and adds the components to the dataset in database.
     """
-
-    def map_commodity(json_dict: Dict) -> EnergyCommodityCreate:
-        """
-        Maps a json dict to a EnergyCommodityCreate object.
-        """
-        json_dict["ref_dataset"] = dataset_id
-        return EnergyCommodityCreate.parse_obj(json_dict)
-
-    commodities: List[EnergyCommodityCreate] = json.load(commodities_file,
-                                                         object_hook=lambda d: map_commodity(d))
-    for commodity in commodities:
-        existing_commodity = crud.energy_commodity.get_by_dataset_and_name(db, dataset_id=dataset_id,
-                                                                           name=commodity.name)
-        if existing_commodity is None:
-            print(f"Commodity {commodity.name} doesn't exists in dataset {dataset_id}. Creating...")
-            crud.energy_commodity.create(db, obj_in=commodity)
-        else:
-            print(f"Commodity {commodity.name} already exists in database. Updating...")
-            update = EnergyCommodityUpdate(**commodity.dict())
-            crud.energy_commodity.update(db, obj_in=update, db_obj=existing_commodity)
-
-
-def retrieve_subfolders(zip_archive: ZipFile, folder_name: str) -> List[str]:
-    """
-    Checks if the folder_name exits inside the zip_archive and converts every sub folder
-    """
-    print(f"Processing folder {folder_name}")
     if folder_name not in zip_archive.namelist():
         # raise ValueError(f"Folder {folder_name}
         print(f"Folder {folder_name} doesn't exists in zip archive. Skipping...")
         return
 
-    for sub_folder in zip_archive.open(folder_name):
-        print(f"Processing {sub_folder}")
-        yield sub_folder
+    # get all sub folder names inside folder_name
+    sub_folder_names = [name for name in zip_archive.namelist() if name.startswith(folder_name)
+                        and name.count("/") == 2
+                        and name.endswith("/")]
 
+    for sub_folder_name in sub_folder_names:
+        json_dict = json.load(zip_archive.open(sub_folder_name + component_file_name))
+        create_or_update(crud_repo, db, map_with_dataset_id(create_model, json_dict, dataset_id))
 
-def process_single_conversion(zip_archive: ZipFile, folder_name: str, dataset_id: int, db: Session):
-    """
-    TODO
-
-    :param zip_archive: Zip archive to process
-    :param folder_name: Name of the folder to process
-    :param dataset_id: ID of the dataset to add the conversions to
-    :param db: Database session
-    """
-
-    def map_conversion(json_dict: Dict) -> EnergyConversionCreate:
-        """
-        Maps a json dict to a EnergyCommodityCreate object.
-        """
-        json_dict["ref_dataset"] = dataset_id
-        return EnergyConversionCreate.parse_obj(json_dict)
-
-    conversion_file = zip_archive.open(folder_name + "/conversion.json")
-    if conversion_file is None:
-        raise ValueError(f"Conversion file not found in {folder_name}.")
-
-    conversion = json.load(conversion_file, object_hook=lambda d: map_conversion(d))
-    existing_conversion = crud.energy_conversion.get_by_dataset_and_name(db, dataset_id=dataset_id,
-                                                                         name=conversion.name)
-    if existing_conversion is None:
-        print(f"Commodity {conversion.name} doesn't exists in dataset {dataset_id}. Creating...")
-        existing_conversion = crud.energy_conversion.create(db, obj_in=conversion)
-    else:
-        print(f"Commodity {conversion.name} already exists in database. Updating...")
-        update = EnergyConversionUpdate(**conversion.dict())
-        existing_conversion = crud.energy_conversion.update(db, obj_in=update, db_obj=existing_conversion)
-
+        # TODO Process all xlsx files in sub_folder_name
