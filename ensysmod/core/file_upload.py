079f244e80674f827ccb7878f753aa1b1515783d
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-21 01:22:48 +0100

    Improved file upload for transmissions

--- ensysmod/core/file_upload.py
+++ ensysmod/core/file_upload.py
@@ -112,7 +112,8 @@ def process_dataset_zip_archive(zip_archive: ZipFile, dataset_id: int, db: Sessi
     transmission_results = process_components_folder(zip_archive, folder_name="transmissions/",
                                                      component_file_name="transmission.json",
                                                      dataset_id=dataset_id, db=db, crud_repo=crud.energy_transmission,
-                                                     create_model=schemas.EnergyTransmissionCreate)
+                                                     create_model=schemas.EnergyTransmissionCreate,
+                                                     matrix_files=True)
     file_results.extend(transmission_results)
 
     if all(file_result.status == FileStatus.OK for file_result in file_results):
@@ -147,7 +148,7 @@ def process_components_folder(zip_archive: ZipFile,
                               dataset_id: int,
                               db: Session,
                               crud_repo: CRUDBaseDependsComponent,
-                              create_model: Type[BaseModel]) -> List[FileUploadResult]:
+                              create_model: Type[BaseModel], matrix_files: bool = False) -> List[FileUploadResult]:
     """
     Processes a folder and adds the components to the dataset in database.
     """
@@ -175,17 +176,89 @@ def process_components_folder(zip_archive: ZipFile,
                                                  message=f"Skipped files in folder {sub_folder_name} due to error."))
             continue
 
-        # check if operationRateFix.xlsx exists in sub_folder_name
-        if sub_folder_name + "operationRateFix.xlsx" in zip_archive.namelist():
-            # process operationRateFix.xlsx
-            file_results.append(process_excel_file(zip_archive.open(sub_folder_name + "operationRateFix.xlsx"),
-                                                   db, dataset_id,
-                                                   json_dict["name"], "fix_operation_rates",
-                                                   crud_repo=crud.operation_rate_fix,
-                                                   create_model=schemas.OperationRateFixCreate))
+        if matrix_files:
+            results = process_sub_folder_matrix_files(zip_archive, sub_folder_name, db, dataset_id, json_dict["name"])
+        else:
+            results = process_sub_folder_files(zip_archive, sub_folder_name, db, dataset_id, json_dict["name"])
+        file_results.extend(results)
 
-        # TODO Add more excel files here
+    return file_results
 
+
+def process_sub_folder_files(zip_archive: ZipFile, sub_folder_name: str, db: Session, dataset_id: int,
+                             component_name: str):
+    file_results: List[FileUploadResult] = []
+
+    # check if operationRateFix.xlsx exists in sub_folder_name
+    if sub_folder_name + "operationRateFix.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_excel_file(zip_archive.open(sub_folder_name + "operationRateFix.xlsx"),
+                                               db, dataset_id,
+                                               component_name, "fix_operation_rates",
+                                               crud_repo=crud.operation_rate_fix,
+                                               create_model=schemas.OperationRateFixCreate))
+
+    # check if operationRateMax.xlsx exists in sub_folder_name
+    if sub_folder_name + "operationRateMax.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_excel_file(zip_archive.open(sub_folder_name + "operationRateMax.xlsx"),
+                                               db, dataset_id,
+                                               component_name, "max_operation_rates",
+                                               crud_repo=crud.operation_rate_max,
+                                               create_model=schemas.OperationRateMaxCreate))
+
+    # check if capacityFix.xlsx exists in sub_folder_name
+    if sub_folder_name + "capacityFix.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_excel_file(zip_archive.open(sub_folder_name + "capacityFix.xlsx"),
+                                               db, dataset_id,
+                                               component_name, "fix_capacities",
+                                               crud_repo=crud.capacity_fix,
+                                               create_model=schemas.CapacityFixCreate))
+
+    # check if capacityMax.xlsx exists in sub_folder_name
+    if sub_folder_name + "capacityMax.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_excel_file(zip_archive.open(sub_folder_name + "capacityMax.xlsx"),
+                                               db, dataset_id,
+                                               component_name, "max_capacities",
+                                               crud_repo=crud.capacity_max,
+                                               create_model=schemas.CapacityMaxCreate))
+    return file_results
+
+
+def process_sub_folder_matrix_files(zip_archive: ZipFile, sub_folder_name: str, db: Session, dataset_id: int,
+                                    component_name: str):
+    file_results: List[FileUploadResult] = []
+
+    # check if capacityFix.xlsx exists in sub_folder_name
+    if sub_folder_name + "capacityFix.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_matrix_excel_file(zip_archive.open(sub_folder_name + "capacityFix.xlsx"),
+                                                      db, dataset_id,
+                                                      component_name, "fix_capacities",
+                                                      crud_repo=crud.capacity_fix,
+                                                      create_model=schemas.CapacityFixCreate))
+
+    # check if capacityMax.xlsx exists in sub_folder_name
+    if sub_folder_name + "capacityMax.xlsx" in zip_archive.namelist():
+        # process operationRateFix.xlsx
+        file_results.append(process_matrix_excel_file(zip_archive.open(sub_folder_name + "capacityMax.xlsx"),
+                                                      db, dataset_id,
+                                                      component_name, "max_capacities",
+                                                      crud_repo=crud.capacity_max,
+                                                      create_model=schemas.CapacityMaxCreate))
+
+    # TODO fix crud for transmission distances first
+    # # check if distances.xlsx exists in sub_folder_name
+    # if sub_folder_name + "distances.xlsx" in zip_archive.namelist():
+    #     # process operationRateFix.xlsx
+    #     file_results.append(process_matrix_excel_file(zip_archive.open(sub_folder_name + "distances.xlsx"),
+    #                                                   db, dataset_id,
+    #                                                   component_name, "distance",
+    #                                                   crud_repo=crud.energy_transmission_distance,
+    #                                                   create_model=schemas.EnergyTransmissionDistanceCreate,
+    #                                                   as_list=False))
     return file_results
 
 
@@ -203,13 +276,51 @@ def process_excel_file(file: TemporaryFile, db: Session, dataset_id: int, compon
     :param create_model: Create model to use
     """
     try:
-        df = pd.read_excel(file)
+        df: pd.DataFrame = pd.read_excel(file)
 
         # for each column, create a time series
         for column in df.columns:
             request_dict = {data_key: df[column].tolist(), "region": column, "component": component_name}
             create_request = map_with_dataset_id(create_model, request_dict, dataset_id)
             create_or_update_time_series(crud_repo, db, create_request)
+        return FileUploadResult(status=FileStatus.OK, file=file.name, message=f"Processed {file.name}")
+    except Exception as e:
+        return FileUploadResult(status=FileStatus.ERROR, file=file.name, message=str(e))
+
+
+def process_matrix_excel_file(file: TemporaryFile, db: Session, dataset_id: int, component_name: str, data_key: str,
+                              crud_repo: CRUDBaseDependsTimeSeries, create_model: Type[BaseModel],
+                              as_list: bool = True) -> FileUploadResult:
+    """
+    Processes an excel file and adds the matrix data (region_from / region_to) to the database.
+
+    :param file: File to process
+    :param db: Database session
+    :param dataset_id: ID of the dataset to add the components to
+    :param component_name: Name of the component to add the time series to
+    :param data_key: Key of the data to add the time series to
+    :param crud_repo: CRUD repository to use
+    :param create_model: Create model to use
+    :param as_list: If true, the data is returned as a list, otherwise as a single float
+    """
+    try:
+        df: pd.DataFrame = pd.read_excel(file)
+
+        # determine the labels (region_from and region_to) of rows and columns
+        regions = df.iloc[:, 0].tolist()
+        df.drop(df.columns[0], axis=1, inplace=True)
+        regions_to = df.columns.tolist()
+
+        # for each element of matrix
+        for i in range(0, len(df.values)):
+            for j in range(0, len(df.values[i])):
+                request_dict = {"region": regions[i], "region_to": regions_to[j], "component": component_name}
+                if as_list:
+                    request_dict[data_key] = [df.values[i][j]]
+                else:
+                    request_dict[data_key] = df.values[i][j]
+                create_request = map_with_dataset_id(create_model, request_dict, dataset_id)
+                create_or_update_time_series(crud_repo, db, create_request)
         return FileUploadResult(status=FileStatus.OK, file=file.name, message=f"Processed {file.name}")
     except Exception as e:
         return FileUploadResult(status=FileStatus.ERROR, file=file.name, message=str(e))
