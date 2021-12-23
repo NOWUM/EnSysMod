308832a989c6ec5040bb86ebb2751080a4954505
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-23 10:11:19 +0100

    Added documentation to commodities

--- ensysmod/schemas/energy_commodity.py
+++ ensysmod/schemas/energy_commodity.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from ensysmod.schemas import Dataset
 
@@ -9,32 +9,32 @@ class EnergyCommodityBase(BaseModel):
     """
     Shared properties for an energy commodity. Used as a base class for all schemas.
     """
-    name: str
-    unit: str
-    description: Optional[str] = None
+    name: str = Field(..., description="Name of the energy commodity", example="Electricity")
+    unit: str = Field(..., description="Unit of the energy commodity", example="GW")
+    description: Optional[str] = Field(None, description="Description of the energy commodity", example="Electricity")
 
 
 class EnergyCommodityCreate(EnergyCommodityBase):
     """
     Properties to receive via API on creation of an energy commodity.
     """
-    ref_dataset: int
+    ref_dataset: int = Field(..., description="Reference to the dataset", example=1)
 
 
 class EnergyCommodityUpdate(EnergyCommodityBase):
     """
     Properties to receive via API on update of an energy commodity.
     """
-    name: Optional[str] = None
-    unit: Optional[str] = None
+    name: Optional[str] = Field(None, description="New Name of the energy commodity", example="Electricity")
+    unit: Optional[str] = Field(None, description="New Unit of the energy commodity", example="GW")
 
 
 class EnergyCommodity(EnergyCommodityBase):
     """
     Properties to return via API for an energy commodity.
     """
-    id: int
-    dataset: Dataset
+    id: int = Field(..., description="Id of the energy commodity")
+    dataset: Dataset = Field(..., description="Dataset object where the energy commodity belongs to")
 
     class Config:
         orm_mode = True
