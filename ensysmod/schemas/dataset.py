f0113ec0b3c2edf6cc5813a611cfbc3fc5b06805
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-23 10:10:31 +0100

    Added documentation to dataset

--- ensysmod/schemas/dataset.py
+++ ensysmod/schemas/dataset.py
@@ -1,14 +1,15 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class DatasetBase(BaseModel):
     """
     Shared properties for a dataset. Used as a base class for all schemas.
     """
-    name: str
-    description: Optional[str] = None
+    name: str = Field(..., description="Name of the dataset", example="2050 Worldwide")
+    description: Optional[str] = Field(None, description="Description of the dataset",
+                                       example="Modeling year 2050 with all countries")
 
 
 class DatasetCreate(DatasetBase):
@@ -22,14 +23,14 @@ class DatasetUpdate(DatasetBase):
     """
     Properties to receive via API on update of a dataset.
     """
-    name: Optional[str] = None
+    name: Optional[str] = Field(None, description="New Name of the dataset", example="2051 Worldwide")
 
 
 class Dataset(DatasetBase):
     """
     Properties to return via API for a dataset.
     """
-    id: int
+    id: int = Field(..., description="Id of the dataset", example=1)
 
     class Config:
         orm_mode = True
