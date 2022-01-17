e5237a26d9376221f64254bff906e14a239c6d9d
Paule <44635962+V3lop5@users.noreply.github.com>
2022-01-17 12:50:40 +0100

    Added dataset 2 with index column Added test case

---
+++ examples/data/dataset-2/README.md
@@ -0,0 +1,2 @@
+# Dataset 2 (Copy of Dataset 1)
+... but with index column in excel files!
--- tests/api/test_zip_upload.py
+++ tests/api/test_zip_upload.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import zipfile
+import pytest
 from typing import Dict
 from zipfile import ZipFile
 
@@ -10,26 +11,27 @@ from sqlalchemy.orm import Session
 from tests.utils import data_generator
 
 
-def get_dataset_1_zip() -> str:
+def get_dataset_zip(folder_name: str) -> str:
     """
     Creates a zip archive from folder structure ../../examples/dataset-1/
     """
     # Create a temporary directory
     temp_dir = tempfile.mkdtemp()
     # create a zip file from the directory
-    zip_file_path = os.path.join(temp_dir, "dataset-1.zip")
+    zip_file_path = os.path.join(temp_dir, f"{folder_name}.zip")
     with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
         project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
         print(f"Project root: {project_root}")
-        for root, dirs, files in os.walk(f"{project_root}/examples/data/dataset-1/"):
-            acr_path = os.path.relpath(root, f"{project_root}/examples/data/dataset-1/")
+        for root, dirs, files in os.walk(f"{project_root}/examples/data/{folder_name}/"):
+            acr_path = os.path.relpath(root, f"{project_root}/examples/data/{folder_name}/")
             zip_file.write(root, acr_path)
             for file in files:
                 zip_file.write(os.path.join(root, file), arcname=os.path.join(acr_path, file))
     return zip_file_path
 
 
-def test_upload_dataset(client: TestClient, db: Session, normal_user_headers: Dict[str, str]):
+@pytest.mark.parametrize("data_folder", ["dataset-1", "dataset-2"])
+def test_upload_dataset(client: TestClient, db: Session, normal_user_headers: Dict[str, str], data_folder: str):
     """
     Test creating a dataset.
     """
@@ -37,7 +39,7 @@ def test_upload_dataset(client: TestClient, db: Session, normal_user_headers: Di
     dataset = data_generator.random_existing_dataset(db)
 
     # Upload a zip file
-    zip_file_path = get_dataset_1_zip()
+    zip_file_path = get_dataset_zip("dataset-1")
 
     # print all the contents of the zip file
     print(f"Zip file contents of {zip_file_path}:")
