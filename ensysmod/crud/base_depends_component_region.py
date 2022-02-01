c09c501a243bc70f9af551c651662e05d774f74f
Paule <44635962+V3lop5@users.noreply.github.com>
2022-02-01 22:41:34 +0100

    Updated create for component region crud's

--- ensysmod/crud/base_depends_component_region.py
+++ ensysmod/crud/base_depends_component_region.py
@@ -1,7 +1,5 @@
-from operator import or_
-from typing import Optional, Generic, List, Type
+from typing import Optional, Generic, List, Union, Dict
 
-import pandas as pd
 from sqlalchemy.orm import Session
 
 from ensysmod import crud
@@ -13,6 +11,7 @@ class CRUDBaseDependsComponentRegion(CRUDBaseDependsDataset, Generic[ModelType, 
     """
     Base class for all CRUD classes that depend on a component and region.
     """
+
     def get_by_component(self, db: Session, *, component_id: int) -> Optional[List[ModelType]]:
         return db.query(self.model) \
             .filter(self.model.ref_component == component_id) \
@@ -24,19 +23,25 @@ class CRUDBaseDependsComponentRegion(CRUDBaseDependsDataset, Generic[ModelType, 
             .filter(self.model.ref_region == region_id) \
             .first()
 
-    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_dict = obj_in.dict()
+    def create(self, db: Session, *, obj_in: Union[CreateSchemaType, Dict]) -> ModelType:
+        if isinstance(obj_in, dict):
+            obj_in_dict = obj_in
+        else:
+            obj_in_dict = obj_in.dict()
 
-        component = crud.energy_component.get_by_dataset_and_name(db, name=obj_in.component,
-                                                                  dataset_id=obj_in.ref_dataset)
-        obj_in_dict['ref_component'] = component.id
+        if 'ref_component' not in obj_in_dict:
+            component = crud.energy_component.get_by_dataset_and_name(db, name=obj_in_dict['component'],
+                                                                      dataset_id=obj_in_dict['ref_dataset'])
+            obj_in_dict['ref_component'] = component.id
 
-        if obj_in.region is not None:
-            region = crud.region.get_by_dataset_and_name(db, name=obj_in.region, dataset_id=obj_in.ref_dataset)
+        if 'region' in obj_in_dict and obj_in_dict['region'] is not None:
+            region = crud.region.get_by_dataset_and_name(db, name=obj_in_dict['region'],
+                                                         dataset_id=obj_in_dict['ref_dataset'])
             obj_in_dict['ref_region'] = region.id
 
-        if obj_in.region_to is not None:
-            region_to = crud.region.get_by_dataset_and_name(db, name=obj_in.region_to, dataset_id=obj_in.ref_dataset)
+        if 'region_to' in obj_in_dict and obj_in_dict['region_to'] is not None:
+            region_to = crud.region.get_by_dataset_and_name(db, name=obj_in_dict['region_to'],
+                                                            dataset_id=obj_in_dict['ref_dataset'])
             obj_in_dict['ref_region_to'] = region_to.id
 
         return super().create(db=db, obj_in=obj_in_dict)
